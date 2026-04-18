b121e67e39871c1789f8338bbae6e6509ff98c21
commitwithabhi <reachabhi2008@gmail.com>
2026-04-18 12:46:01 +0530

    folder commit

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-04-11T11:31:08.083339700+00:00
+-- finished_at: 2026-04-11T11:31:08.187263100+00:00
+-- elapsed: 103ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c3a2f4-0001-c729-0000-000d39169431
+-- desc: not provided
+select 1 as id;
